c5ec49ce2099ec05156f5319dbddf8bf836f7b39
= <=>
2026-07-21 19:39:39 +0300

    feat: add headers to models

--- dbt/models/bronze/category_translation.sql
+++ dbt/models/bronze/category_translation.sql
@@ -3,4 +3,4 @@
 select
     *,
     '{{ run_started_at }}' as _loaded_at
-from csv.`/Volumes/semantic-demo-catalog/bronze/olist_csvs/product_category_name_translation.csv`
+from csv.`/Volumes/semantic-demo-catalog/bronze/olist_csvs/product_category_name_translation.csv` (header => true)
